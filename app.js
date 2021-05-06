980d0a942e040a9d66d67c22eab1a4c79d0b57e1
arupsarkar <sarkar.arup@gmail.com>
2021-05-06 13:18:15 -0400

    Exposed swagger.json via api route

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ const swaggerSchema = require('./routes/swagger/swagger.json');
 const app = express();
 
 
+// Setup express static middleware to look for files in the api directory for all requests starting with /api
+app.use('/api', express.static('./routes/swagger') , function(req, res){
+    // Optional 404 handler
+    res.status(404);
+    res.json({error:{code:404}})
+});
+
 app.use('/services/onedegree', onedegreeRouter);
 app.use('/ext-svc/swagger', swaggerDocRouter);
-app.use('/ext-svc/swagger/schema', swaggerSchema);
+// app.use('/ext-svc/swagger/schema', swaggerSchema);
 
 
 module.exports = app;
