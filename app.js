aa2d234f5d6f122000607123f6b7d3083f7601e3
arupsarkar <sarkar.arup@gmail.com>
2021-05-06 12:33:29 -0400

    Exposed schema as an URL

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const path = require('path');
 
 const onedegreeRouter = require('./routes/services/onedegree/opportunities');
 const swaggerDocRouter = require('./routes/swagger/swagger');
+const swaggerSchema = require('./routes/swagger/swagger.json');
 const app = express();
 
 
 app.use('/services/onedegree', onedegreeRouter);
 app.use('/ext-svc/swagger', swaggerDocRouter);
+app.use('/ext-svc/swagger/schema', swaggerSchema);
 
 
 module.exports = app;
